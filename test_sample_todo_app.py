7a53b3bf80ec5f6d35bc647fefd32c897adbef95
gkyurkchiyski <g.kyurkchiyski@gmail.com>
2025-04-03 18:56:54 +0300

    4th commit

--- test_sample_todo_app.py
+++ test_sample_todo_app.py
@@ -1,37 +1,28 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import os
 import json
 
 url = os.getenv("LT_HUB_URL")
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")  # Uncomment if you want the browser to run in headless mode
-
+capabilities = {
+    "build" : os.getenv("LT_BUILD_NAME"),
+    "name" : "Quick Test",
+    "platform" : "Windows 10",
+    "browserName" : "Chrome",
+    "version" : "88.0",
+    "resolution" : "1920x1080",
+    "tunnel" : True
+}
 driver = webdriver.Remote(
-    command_executor=url,
-    options=options
+    desired_capabilities= capabilities,
+    command_executor= url
 )
-
 driver.get("http://localhost:8081/")
+driver.find_element_by_name("li3").click()
 
-# Increase the wait time and wait for visibility of the element
-try:
-    li3_element = WebDriverWait(driver, 20).until(
-        EC.visibility_of_element_located((By.NAME, "li3"))
-    )
-    li3_element.click()
-    
-    textbox = driver.find_element(By.ID, "sampletodotext")
-    textbox.send_keys("Testing")
-    driver.find_element(By.ID, "addbutton").click()
-
-    # Assert that the element doesn't contain the "No results found." text
-    assert "No results found." not in driver.page_source
-
-    driver.execute_script("lambda-status=passed")
-finally:
-    driver.quit()
+textbox = driver.find_element_by_id("sampletodotext")
+textbox.send_keys("Testing")
+driver.find_element_by_id("addbutton").click()
+assert "No results found." not in driver.page_source
+driver.execute_script("lambda-status=passed")
+driver.quit()
