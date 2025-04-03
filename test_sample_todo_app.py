3295906fa8854901b1fdd3c5bb3b768132d679a1
gkyurkchiyski <g.kyurkchiyski@gmail.com>
2025-04-03 18:28:01 +0300

    3rd commit

--- test_sample_todo_app.py
+++ test_sample_todo_app.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import os
 import json
@@ -20,11 +21,14 @@ driver = webdriver.Remote(
 )
 
 driver.get("http://localhost:8081/")
-driver.find_element_by_name("li3").click()
 
-textbox = driver.find_element_by_id("sampletodotext")
+# Update the deprecated method to use the 'By' class
+driver.find_element(By.NAME, "li3").click()
+
+textbox = driver.find_element(By.ID, "sampletodotext")
 textbox.send_keys("Testing")
-driver.find_element_by_id("addbutton").click()
+driver.find_element(By.ID, "addbutton").click()
+
 assert "No results found." not in driver.page_source
 driver.execute_script("lambda-status=passed")
 driver.quit()
